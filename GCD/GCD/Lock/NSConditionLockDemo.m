3eea1cca941ff89730906bfce72f6eb0930d3f32
wbb <wbb180625@outlook.com>
2020-12-12 11:18:24 +0800

    添加注释

--- GCD/GCD/Lock/NSConditionLockDemo.m
+++ GCD/GCD/Lock/NSConditionLockDemo.m
@@ -37,22 +37,23 @@
     
     NSLog(@"__one");
     sleep(1);
-    
+    // 解锁 并将内部条件 设置为 2
     [self.conditionLock unlockWithCondition:2];
 }
 
 - (void)__two
-{
+{   // 当内部条件是 2 时 加锁
     [self.conditionLock lockWhenCondition:2];
     
     NSLog(@"__two");
     sleep(1);
-    
+    // 解锁 并将内部条件 设置为 3
     [self.conditionLock unlockWithCondition:3];
 }
 
 - (void)__three
 {
+    // 当内部条件是 3 时 加锁
     [self.conditionLock lockWhenCondition:3];
     
     NSLog(@"__three");
